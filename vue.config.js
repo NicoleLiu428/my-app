a48d3dd62ef110e298e1900c7ad6a6acd3344368
NicoleLiu428 <aaronlxning@yahoo.com>
2024-08-04 00:50:52 +0800

    Update vue.config.js
    add  publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false//關閉eslink校驗
+  lintOnSave: false,//關閉eslink校驗
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-app/'
+    : '/'
 })
